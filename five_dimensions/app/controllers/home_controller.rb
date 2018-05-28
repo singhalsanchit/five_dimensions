401177dfa0568bb2a8ba77ac462aca9e9bd58c1e
sanchit <sanchit.singhal@berylsystems.com>
2018-05-28 22:39:18 +0530

    added sendgrid

--- five_dimensions/app/controllers/home_controller.rb
+++ five_dimensions/app/controllers/home_controller.rb
@@ -1,15 +1,26 @@
 class HomeController < ApplicationController
-
+	require 'sendgrid-ruby'
+	include SendGrid
+	
 	def index
 		
 	end
 
 	def send_request
-		if params
-			UserNotifier.send_signup_email(params).deliver
-			redirect_to root_path
-		else
-			redirect_to root_path
+		if params[:name] and params[:email] and params[:message]
+			from = Email.new(email: 'events@5dimensions.co.in')
+			to = Email.new(email: params[:email])
+			subject = "Query from #{params[:name]}"
+			message = "Hi Guys, a query has been raised by #{params[:name]} (#{params[:email]}) as #{params[:message]} ."
+			content = Content.new(type: 'text/plain', value: message)
+			mail = Mail.new(from, subject, to, content)
+			
+			# debugger
+
+			sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+			response = sg.client.mail._('send').post(request_body: mail.to_json)
+			puts response.status_code
 		end
+		redirect_to root_path
 	end
 end
--- five_dimensions/config/environment.rb
+++ five_dimensions/config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'singhal.sanchit1@gmail.com',
-  :password => '$Anchit92',
-  :domain => 'fivedimensions.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
